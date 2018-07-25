063708908c20e43327fef77bcabe7fa64f661add
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-25 14:32:25 -0600

    Nikki timed assessment

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,54 @@
+require 'pry'
 class Pantry
+    attr_reader :stock,
+                :shopping_list,
+                :cookbook
+  def initialize
+    @stock = Hash.new(0)
+    @shopping_list = Hash.new(0)
+    @cookbook = []
+  end
+
+  def stock_check(item)
+    @stock[item]
+  end
+
+  def restock(item, amount)
+    @stock[item] += amount
+  end
+
+  def add_to_shopping_list(r)
+    r.ingredients.map do |item, amount|
+      @shopping_list[item] += amount
+    end
+  end
+
+  def print_shopping_list
+    printed_list = []
+    @shopping_list.each do |item, amount|
+      printed_list << "* #{item}: #{amount}"
+    end
+    printed_list.join("\n")
+  end
+
+  def add_to_cookbook(r)
+    @cookbook << r
+  end
+
+  def what_can_i_make
+    have_enough_ingredients.map do |r|
+      r.name
+    end
+  end
+
+  def have_enough_ingredients
+    @cookbook.find_all do |r|
+      r.ingredients.all? do |item, amount|
+        @stock[item] >= amount
+      end
+    end
+  end
+
+
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,153 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+
+  def test_pantry_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_stock_starts_empty
+    pantry = Pantry.new
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_can_check_stock
+    pantry = Pantry.new
+    pantry.stock_check("Cheese")
+    assert_equal 0,   pantry.stock_check("Cheese")
+  end
+
+  def test_can_restock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.stock_check("Cheese")
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_keep_restocking
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.stock_check("Cheese")
+    pantry.restock("Cheese", 20)
+    pantry.stock_check("Cheese")
+
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
+
+  def test_can_build_recipe
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+    assert_equal ({}), r.ingredients
+  end
+
+  def test_it_can_add_ingredients_to_list
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+    r.add_ingredient("Cheese", 20)
+    r.add_ingredient("Flour", 20)
+    pantry.add_to_shopping_list(r)
+    expected = {"Cheese" => 20, "Flour" => 20}
+    assert_equal expected, pantry.shopping_list
+  end
+
+  def test_it_can_add_more_ingredients_to_list
+      pantry = Pantry.new
+      recipe_p = Recipe.new("Cheese Pizza")
+      recipe_p.add_ingredient("Cheese", 20)
+      recipe_p.add_ingredient("Flour", 20)
+
+      recipe_s = Recipe.new("Spaghetti")
+      recipe_s.add_ingredient("Spaghetti Noodles", 10)
+      recipe_s.add_ingredient("Marinara Sauce", 10)
+      recipe_s.add_ingredient("Cheese", 5)
+
+      pantry.add_to_shopping_list(recipe_p)
+      pantry.add_to_shopping_list(recipe_s)
+
+      expected = {
+        "Cheese" => 25,
+        "Flour" => 20,
+        "Spaghetti Noodles" => 10,
+        "Marinara Sauce" => 10
+      }
+      assert_equal expected, pantry.shopping_list
+  end
+
+  def test_can_print_shopping_list
+    pantry = Pantry.new
+    recipe_p = Recipe.new("Cheese Pizza")
+    recipe_p.add_ingredient("Cheese", 20)
+    recipe_p.add_ingredient("Flour", 20)
+
+    recipe_s = Recipe.new("Spaghetti")
+    recipe_s.add_ingredient("Spaghetti Noodles", 10)
+    recipe_s.add_ingredient("Marinara Sauce", 10)
+    recipe_s.add_ingredient("Cheese", 5)
+
+    pantry.add_to_shopping_list(recipe_p)
+    pantry.add_to_shopping_list(recipe_s)
+
+    expected = "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10"
+    assert_equal expected, pantry.print_shopping_list
+
+  end
+
+  def test_it_can_add_to_cookbook
+    pantry = Pantry.new
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    assert [r1, r2, r3], pantry.cookbook
+  end
+
+  def test_it_can_make_reccomendations
+    pantry = Pantry.new
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Cucumbers", 120)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+
+    expected = ["Pickles", "Peanuts"]
+
+    assert_equal expected, pantry.what_can_i_make
+  end
+
+
+
 
 end
